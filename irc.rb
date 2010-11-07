655aa405671a38aae37eaa70e9f4231021cf2f7d
kyoendo <postagie@gmail.com>
2010-11-08 08:32:39 +0900

    some refactor

--- irc.rb
+++ irc.rb
@@ -4,8 +4,8 @@ String.send(:include, Term::ANSIColor)
 
 str = 'Ruby is not Gem!'
 
-c = %w(red blue yellow green)
-puts %w(Hello, try interactive colors!).map { |w| w.send(c.shift) }.join(" ")
+C = %w(red green yellow blue magenta cyan white)
+puts %w(Hello, try interactive colors!).map.with_index { |w, i| w.send(C[i]) }.join(" ")
 
 while line = Readline.readline("> ", true)
   begin
@@ -19,11 +19,11 @@ while line = Readline.readline("> ", true)
     when /^(help|h|colors|attrs)$/
       print "input one or more attributes from followings: ex. bold red on_green\n\n"
       puts Term::ANSIColor.attributes.map { |attr| "#{attr}".send(attr) }.join(" ")
-      print "\nto set a string, input string with prepend '='. ex. = Color is fun!\n"
+      print "\nto set a string, input string with prepend '='. ex. = Is Ruby a Jam?\n"
+      print "or try 'rainbow' to fun!\n"
     when /^rainbow$/
-      c = %w(red green yellow blue magenta cyan white)
-      s = [str.chars, str.split(/\b/)][rand(2)]
-      puts s.inject("") { |mem, chr| mem << chr.send(c[rand(c.length)]) }
+      s = [str.chars, str.split(/\b/), str.split(/\b/).reverse][rand(3)]
+      puts s.inject("") { |mem, chr| mem << chr.send(C[rand(C.length)]) }
     else
       puts line.split(/[,\s]+/).inject(str) { |mem, color| mem.send color }
     end
